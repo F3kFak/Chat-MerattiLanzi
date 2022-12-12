566bbf0d457eae276f8761bff6d6be5ca97f860f
MerattiGiacomo <merattigiacomo@gmail.com>
2022-12-12 16:54:53 +0100

    finta la notifica del client

--- server/src/main/java/it/fi/itismeucci/chat/ClientHandler.java
+++ server/src/main/java/it/fi/itismeucci/chat/ClientHandler.java
@@ -23,7 +23,6 @@ import com.fasterxml.jackson.databind.ObjectMapper;
  */
 public class ClientHandler extends Thread {
 
-
     private String nomeUtente;
     private Socket socket;
     private BufferedReader input;
@@ -33,7 +32,7 @@ public class ClientHandler extends Thread {
     private String mexRicevuto;
     private Messaggio utente;
 
-    public ClientHandler(){
+    public ClientHandler() {
         this.input = new BufferedReader(input);
         this.output = new DataOutputStream(output);
         this.objectMapper = new ObjectMapper();
@@ -47,130 +46,142 @@ public class ClientHandler extends Thread {
 
     @Override
     public void run() {
-        //si effettua la registrazione
-        registrazione();
-        //il client è entrato a far parte della chat
-        for ( ClientHandler c : ServerStr.listaClient) {
-            mexInviato.setMittente("Server");
-            mexInviato.setCorpo(nomeUtente + " si è unito alla chat!" + '\n');
+        // si effettua la registrazione
+        try {
+            registrazione();
+        } catch (IOException e) {
+            System.out.println(e);
+            messaggioErrore("Errore di registrazione");
         }
-    /* 
-        1) Login
-
-        do
-            leggi nome utente 
-
-            invia Messaggio
-
-            ricevimessaggio
-
-        while ricevuto ok
-
-        this.nomeUtente = .....
-
-        client va aggiunto alla lista
-
-
-        ClientHandler.listaClient.add(this); 
-
-
-        // chat
-
-        ricevimessaggio
-
-        switch (tipo) {
-
-            scrivi a tutti
-
-            per tutti i client ClientHandler.listaClient c
-            
-
-            // si sta utilizzando l'istanza che gestisce i vari clien
-            // utilizzanod il thread del client che ha inviato il messaggio
-            c.inviaMessaggio
-
-
-            -----------------------
-
-            scrivi a uno
-
-            per tutti i client ClientHandler.listaClient c
-            cercando che c.nomeUtente = al destinatario
-            c.invia messaggio
-
-            
-
-*/
+        //si notifica del nuovo client in chat
+        try{
+            notificaClients();
+        }
+        catch(Exception e){
+            System.out.println(e);
+            messaggioErrore("Errore nella notificazione del nuovo client")
+        }
+        /*
+         * 1) Login
+         * 
+         * do
+         * leggi nome utente
+         * 
+         * invia Messaggio
+         * 
+         * ricevimessaggio
+         * 
+         * while ricevuto ok
+         * 
+         * this.nomeUtente = .....
+         * 
+         * client va aggiunto alla lista
+         * 
+         * 
+         * ClientHandler.listaClient.add(this);
+         * 
+         * 
+         * // chat
+         * 
+         * ricevimessaggio
+         * 
+         * switch (tipo) {
+         * 
+         * scrivi a tutti
+         * 
+         * per tutti i client ClientHandler.listaClient c
+         * 
+         * 
+         * // si sta utilizzando l'istanza che gestisce i vari clien
+         * // utilizzanod il thread del client che ha inviato il messaggio
+         * c.inviaMessaggio
+         * 
+         * 
+         * -----------------------
+         * 
+         * scrivi a uno
+         * 
+         * per tutti i client ClientHandler.listaClient c
+         * cercando che c.nomeUtente = al destinatario
+         * c.invia messaggio
+         */
     }
 
-
-        
-
-
-
-
-
-
-        //ciclo+
-
-    public void registrazione() throws IOException{
+    public void registrazione() throws IOException {
         boolean exists = false;
         do {
             System.out.println("Nuovo Thread creato");
-            //ricevo il messaggio e lo deserializzo
+            // ricevo il messaggio e lo deserializzo
             utente = riceviMessaggio(input.readLine());
             for (ClientHandler c : ServerStr.listaClient) {
-                //controllo che il nome utente non sia esistente
-                if(utente.getMittente().equals(c.getNomeUtente())){
-                    //var impostata su true per ripetere il ciclo
+                // controllo che il nome utente non sia esistente
+                if (utente.getMittente().equals(c.getNomeUtente())) {
+                    // var impostata su true per ripetere il ciclo
                     exists = true;
-                    //messaggio di errore
+                    // messaggio di errore
                     mexInviato.setMittente("Server");
                     mexInviato.setCorpo("Connessione rifiutata, client già esistente");
-                    //invio il messaggio al client
+                    // invio il messaggio al client
                     inviaMessaggio(mexInviato);
                     break;
                 }
             }
         } while (exists);
-        //il client non è un doppione
-        //aggiungo il client alla lista
+        // il client non è un doppione
+        // aggiungo il client alla lista
         ServerStr.listaClient.add(this);
-        //imposto il nome del client
+        // imposto il nome del client
         nomeUtente = utente.getMittente();
     }
 
-    public void inviaMessaggio(Messaggio mexInviato) throws IOException{
-        //serializzo
+    public void notificaClients() {
+        // il client è entrato a far parte della chat e lo notifica
+        for (ClientHandler c : ServerStr.listaClient) {
+            try {
+                mexInviato.setMittente("Server");
+                mexInviato.setCorpo(nomeUtente + " si è unito alla chat!" + '\n');
+                c.inviaMessaggio(mexInviato);
+            } catch (IOException e) {
+                messaggioErrore("Errore nell'invio del messaggio dal server");
+            }
+        }
+    }
+
+    public void messaggioErrore(String err) {
+        mexInviato.setMittente("Server");
+        mexInviato.setCorpo(err);
+        System.out.println(err);
+    }
+
+    public void inviaMessaggio(Messaggio mexInviato) throws IOException {
+        // serializzo
         String stringaSerializzata = objectMapper.writeValueAsString(mexInviato);
-        //scrivo il messaggio
+        // scrivo il messaggio
         output.writeBytes(stringaSerializzata + '\n');
     }
 
     public Messaggio riceviMessaggio(String mexRicevuto) throws JsonMappingException, JsonProcessingException {
-        //deserializzo
+        // deserializzo
         Messaggio stringaDeserializzata = objectMapper.readValue(mexRicevuto, Messaggio.class);
-        //ritorno il l'istanza
+        // ritorno il l'istanza
         return stringaDeserializzata;
     }
 
     /*
-    public String serializzazionemex(Messaggio mexInviato) throws JsonProcessingException{
-        String stringaSerializzata = objectMapper.writeValueAsString(mexInviato);
-        return stringaSerializzata;
-    }
-
-    public Messaggio deserializzazionemex(String mexRicevuto) throws JsonMappingException, JsonProcessingException{
-
-        Messaggio stringaDeserializzata = objectMapper.readValue(mexRicevuto, Messaggio.class);
-        return stringaDeserializzata;
-    }
-    */
+     * public String serializzazionemex(Messaggio mexInviato) throws
+     * JsonProcessingException{
+     * String stringaSerializzata = objectMapper.writeValueAsString(mexInviato);
+     * return stringaSerializzata;
+     * }
+     * 
+     * public Messaggio deserializzazionemex(String mexRicevuto) throws
+     * JsonMappingException, JsonProcessingException{
+     * 
+     * Messaggio stringaDeserializzata = objectMapper.readValue(mexRicevuto,
+     * Messaggio.class);
+     * return stringaDeserializzata;
+     * }
+     */
 }
 
-
-
-
-
-
-//Thread sia un istanza di oggetto che un thread
+// Thread sia un istanza di oggetto che un thread
